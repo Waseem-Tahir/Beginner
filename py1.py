ade7c81b56024d797503404022f29d5e8654dba5
Waseem-Tahir <waseemtahir262@gmail.com>
2026-08-30 12:05:27 +0500

    This program find the avarage of three numbers

--- py1.py
+++ py1.py
@@ -1,1 +1,5 @@
-print("hellow world")
+# print("hellow world")                    # this is my first day program
+# ______________________________________________
+
+a,b,c=input("enter three no's seperated by ',' to find their avarage : ").split(",")
+print(f"avarage of the no's is {(int(a)+int(b)+int(c))/3}")
