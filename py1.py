ad9f6ae82c6a474cd03268eff468be8459f1ce3e
Waseem-Tahir <waseemtahir262@gmail.com>
2026-08-30 12:11:49 +0500

    add this program also

--- py1.py
+++ py1.py
@@ -1,5 +1,8 @@
-# print("hellow world")                    # this is my first day program
-# ______________________________________________
-
-a,b,c=input("enter three no's seperated by ',' to find their avarage : ").split(",")
-print(f"avarage of the no's is {(int(a)+int(b)+int(c))/3}")
+# print("hellow world")                    # this is my first program
+# _____________________________________________
+                                           #this is my second program
+# a,b,c=input("enter three no's seperated by ',' to find their avarage : ").split(",") 
+# print(f"avarage of the no's is {(int(a)+int(b)+int(c))/3}")
+_______________________________________________
+                                            #this is my third program
+                                        
